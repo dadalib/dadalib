de94fd783ece4337cd218930f1b9bc3d0a3e999e
2104dalibioulleanzul <2104dalibioulleanzul@PC9778.domepfc.net>
2017-12-07 20:57:14 +0100

    reset index.php

--- dadalib/index.php
+++ dadalib/index.php
@@ -1,3 +1,8 @@
+
+<?php
+print_r($_GET);
+var_dump($_POST);
+?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -7,12 +12,36 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>GH</title>
+        <title></title>
     </head>
     <body>
-        <?php
-        echo " I am gooooooooor";
-        // put your code here
-        ?>
+       <!-- <form action="contact_controller.php" method="POST">
+            input
+        </form>-->
+        
+        <div>
+            <p>
+                " Nous avons bien reçu votre demande"
+                " "<br>
+                <?php
+                // <?php
+                // put your code here
+                //reafficher à l' ecran
+                //echo " ECHOOOOOOOO ";
+                
+                echo " Nom : ";
+                echo $_POST["nom"];
+                echo " Prenom : ";
+                echo $_POST["preNom"];
+                ?>
+
+
+            </p>
+
+
+        </div>
+
+
     </body>
 </html>
+
